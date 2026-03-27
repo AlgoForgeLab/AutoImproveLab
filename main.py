64276c0af0f682a84f8f49156fa03652e3c5af68
AlgoForge Labs <inder16092007@gmail.com>
2026-03-27 20:37:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+from strategy.macd_strategy import generate_signal
+from strategy.trend_strategy import generate_trend
+
 print("AutoImproveLab started")
 
 def run():
     print("Framework ready")
+    print("MACD:", generate_signal())
+    print("Trend:", generate_trend())
 
 if __name__ == "__main__":
     run()
